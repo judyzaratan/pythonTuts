803ff654635b66c44d59b5aecef7605f55998504
mochicat8 <mochicat8@gmail.com>
2015-09-29 23:20:49 -0700

    adds changes

--- rename_files.py
+++ rename_files.py
@@ -4,4 +4,15 @@ def rename_files():
     #(1) get file names from a folder
     file_list = os.listdir("/Users/Judy/Documents/Coding/Tutorials/Udacity/Programming Foundations w Python/prank")
     print(file_list)
+    saved_path = os.getcwd()
+    os.chdir("/Users/Judy/Documents/Coding/Tutorials/Udacity/Programming Foundations w Python/prank")
+    print(saved_path)
+
+    #(2) for each file, rename filename
+    for file_name in file_list:
+        os.rename(file_name, file_name.translate(None, "0123456789"))
+        #translate removes numbers in a string
+
+    os.chdir(saved_path)
+
 rename_files()
