5cd16c38b33cda60058d1bd744d2b848adf75de2
mochicat8 <mochicat8@gmail.com>
2015-10-18 00:46:57 -0700

    adds personal items

--- Lesson3/movies/entertainment_center.py
+++ Lesson3/movies/entertainment_center.py
@@ -1,25 +1,35 @@
 import media
+import fresh_tomatoes
 
-toy_story = media.Movie("Toy Story",
-                        "A story of a boy and his toys that come to life",
-                        "http://vignette4.wikia.nocookie.net/disney/images/4/4c/Toy-story-movie-posters-4.jpg/revision/latest?cb=20140816182710",
-                        "https://www.youtube.com/watch?v=4KPTXpQehio")
+silver_linings = media.Movie("Silver Linings Playbook",
+                        "A romantic comedy about a man who tries to rebuild his life and relationships after a stint in a mental hospital",
+                        "http://www.impawards.com/2012/posters/silver_linings_playbook.jpg",
+                        "https://www.youtube.com/watch?v=Lj5_FhLaaQQ")
 #calls init function to create a new movie
-
-#print (toy_story.storyline)
-
-
-avatar = media.Movie("Avatar",
-                     "A marine on an alien planet",
-                     "http://moviecultists.com/wp-content/uploads/2009/11/avatar-poster.jpg",
-                     "https://www.youtube.com/watch?v=cRdxXPV9GNQ")
-
-#print(avatar.storyline)
-#avatar.show_trailer()
-
 
 guardians = media.Movie("Guardians of the Galaxy",
                        "A comic based movie",
                        "http://static2.hypable.com/wp-content/uploads/2014/05/guardians-of-the-galaxy-poster-new-HD.jpg",
                        "https://www.youtube.com/watch?v=GQRqShJGIJc")
-guardians.show_trailer()
+
+into_darkness = media.Movie("Star Trek Into Darkness",
+                     "Crew of Starship Enterprise deal with a terrorist who has a personal vendetta against Spock ",
+                     "http://www.impawards.com/2013/posters/star_trek_into_darkness_ver2_xlg.jpg",
+                     "https://www.youtube.com/watch?v=r5gdbUC9mWU")
+
+wall_e = media.Movie("Wall-E",
+                    "A story about a little robot who changes the Universe",
+                    "http://www.impawards.com/2008/posters/wall_e.jpg",
+                    "https://www.youtube.com/watch?v=alIq_wG9FNk")
+lego = media.Movie("The Lego Movie",
+                    "A computer animated movie about the Lego Universe",
+                    "http://vignette4.wikia.nocookie.net/transcripts/images/e/ea/LEGO-Movie-Poster-2014-HIgh-Resolution.jpg/revision/latest?cb=20150313225134",
+                    "https://www.youtube.com/watch?v=fZ_JOBCLF-I")
+
+batman = media.Movie("Batman Begins",
+                     "The story of how Bruce Wayne, a young billionaire, becomes a crime-fighting superhero for the city of Gotham",
+                     "http://nsamovie.com/wp-content/uploads/2013/01/batman_begins_teaser_c.jpg",
+                     "https://www.youtube.com/watch?v=vak9ZLfhGnQ")
+
+movies = [silver_linings, guardians, into_darkness, wall_e, lego, batman]
+fresh_tomatoes.open_movies_page(movies)
